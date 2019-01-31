2a1dfd9325e80f845ffa0bac45d48a8c538cf5ec
Francesco Bianco <info.francescobianco@gmail.com>
2019-01-31 18:11:07 +0100

    Updates

---
+++ precache-manifest.2c7bec750121b23402346253c3e85415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "533d0e7db7c3a5ca4bed",
+    "url": "/static/js/main.533d0e7d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "533d0e7db7c3a5ca4bed",
+    "url": "/static/css/main.3573977b.chunk.css"
+  },
+  {
+    "revision": "e343a0cc24150d917099547d2d906a9c",
+    "url": "/index.html"
+  }
+];
