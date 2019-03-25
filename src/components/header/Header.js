c4efae334452d688a79e226e38e0ab5d17bc879b
39ro <giovannirigacs@gmail.com>
2019-03-25 21:13:57 +0000

    # Landing page + Youtube logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+import './Header.css';
+
+import yt from '../../assets/yt_logo_mono_light.png';
 
 class Header extends Component {
     render() {
         return (
             <header className="App-header">
-                Golden Comrades
+                <h1>Golden Comrades</h1>
+                <a href="https://www.youtube.com/channel/UC0nuidGQ3IG1JloW4_fC3Kg" target="_blank">
+                    <img src={yt} alt="YouTube" className="yt_logo" style={{'marginTop': '10px'}}/>
+                </a>
             </header>
         );
     }
